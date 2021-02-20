f46c9e20a0ac1bb8c0687740f36b4cad011fbd9d
LexDX <fredom100000@yandex.ru>
2021-02-20 12:19:18 +0300

    Add Angular template

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular';
+  counter: number = 0;
+  buttonText: string = "Click count: 0";
+
+  constructor(){
+    this.onClick=this.onClick.bind(this);
+  }
+  
+  onClick(e:any){
+    this.counter++;
+    this.buttonText = "Click count: " + this.counter;
+  }
+}
