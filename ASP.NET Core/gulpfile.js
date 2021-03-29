61720b86fd91db09a212bcbe0dd0252756b21c98
Tom <artem.kurchenko@devexpress.com>
2021-03-29 17:52:26 +0300

    Update gulpfile.js

--- ASP.NET Core/gulpfile.js
+++ ASP.NET Core/gulpfile.js
@@ -1,9 +1,10 @@
 /// <binding Clean='clean' />
-var gulp = require("gulp"),
+const gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat");
 
-var paths = {
+
+const paths = {
     webroot: "./wwwroot/"
 };
 
@@ -12,16 +13,63 @@ paths.concatCssDest = paths.webroot + "css/vendor.css";
 paths.fonts = paths.webroot + "css/fonts";
 paths.icons = paths.webroot + "css/icons";
 
-let scripts = ["node_modules/jquery/dist/jquery.js",
-    "node_modules/bootstrap/dist/js/bootstrap.js",
-    "node_modules/devextreme/dist/js/dx.all.js",
-    "node_modules/devextreme/aspnet.js",
-    "node_modules/devextreme-aspnet-data/js/dx.aspnet.data.js"];
+let defaultScripts = [
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/bootstrap/dist/js/bootstrap.min.js"],
+    exportScripts = [
+        "node_modules/jszip/dist/jszip.min.js",
+        "node_modules/exceljs/dist/exceljs.min.js",
+        "node_modules/file-saver/FileSaver.min.js"
+    ],
+    quillScripts = ["node_modules/devextreme-quill/dist/dx-quill.min.js",
+        "node_modules/turndown/dist/turndown.js",
+        "node_modules/showdown/dist/showdown.js"
+    ],
+    diagramScript = ["node_modules/devexpress-diagram/dist/dx-diagram.min.js"],
+    ganttScript = ["node_modules/devexpress-gantt/dist/dx-gantt.min.js"],
+    dxtScripts = [
+        "node_modules/devextreme/dist/js/dx.all.js",
+        "node_modules/devextreme/aspnet.js",
+        "node_modules/devextreme-aspnet-data/js/dx.aspnet.data.js"];
 
-let styles = [
+
+let dxtStyles = [
     "node_modules/bootstrap/dist/css/bootstrap.css",
     "node_modules/devextreme/dist/css/dx.common.css",
-    "node_modules/devextreme/dist/css/dx.material.blue.light.compact.css"];
+    "node_modules/devextreme/dist/css/dx.material.blue.light.compact.css"],
+    ganttStyles = ["node_modules/devexpress-gantt/dist/dx-gantt.min.css"],
+    diagramStyles = ["node_modules/devexpress-diagram/dist/dx-diagram.min.css"];
+
+
+/* default configuration */
+
+let styles = dxtStyles;
+let scripts = [...defaultScripts, ...dxtScripts];
+
+/* // Gantt
+scripts = [...defaultScripts, ...ganttScript, ...dxtScripts];
+styles = [...styles, ...ganttStyles];  
+*/
+
+/* // Diagram 
+scripts = [...defaultScripts, ...diagramScript, ...dxtScripts]; 
+styles = [...styles, ...diagramStyles];
+*/
+
+/* // HtmlEditor 
+scripts = [...defaultScripts, ...quillScripts, dxtScripts];
+*/
+
+/* // Export 
+scripts = [...exportScripts, ...scripts];
+*/
+
+ /* // Full bundle 
+
+scripts = [...exportScripts, ...defaultScripts, ...ganttScript, ...diagramScript, ...quillScripts, ...dxtScripts];
+styles = [...dxtStyles, ...diagramStyles, ...ganttStyles];
+
+*/
 
 
 gulp.task("clean:js", function (cb) {
@@ -40,7 +88,9 @@ gulp.task("clean:fonts", function (cb) {
 
 gulp.task("clean", gulp.series(["clean:js", "clean:css", "clean:icons", "clean:fonts"]));
 
+
 gulp.task("add:js", function () {
+
     return gulp.src(scripts, { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(gulp.dest("."));
