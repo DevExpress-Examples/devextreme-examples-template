4549066360d7416ff4d83007c222f07094d00ced
Tom <artem.kurchenko@devexpress.com>
2023-02-05 18:45:49 +0400

    Add Vue

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,28 +1,9 @@
-<template>
-  <Home/>
-</template>
-
-<script>
-
-import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css'
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
+<script setup lang="ts">
+import { RouterView } from "vue-router";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 50px 50px;
-  width: 90vh;
-}
-</style>
+<template>
+    <div class="main">
+        <RouterView />
+    </div>
+</template>
