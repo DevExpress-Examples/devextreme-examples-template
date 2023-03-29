5f9837cd7e768a824ceaccdbfb9c3136536e8557
JustSoldier <aag.msu@gmail.com>
2023-03-29 17:33:17 +0400

    datagrid-dynamic-column-jquery

--- README.md
+++ README.md
@@ -3,17 +3,11 @@
 [![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)](https://supportcenter.devexpress.com/ticket/details/T1129779)
 [![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
 <!-- default badges end -->
-# DevExtreme Examples Template
+# How to handle different value types with editCellTemplate for the same column
 
-This is the repository template for creating new examples. 
+This example describes a typical scenario where users want to have a dynamic column. For instance a column must handle strings/numbers/dates etc.
 
-Use **_Product_ for DevExtreme - _Task_** template for a title. 
-
-Describe the solved task in this section.
-
-Put a screenshot/gif that illustrates the result here.
-
-Then, add implementation details (steps, code snippets, and other technical information in a free form), or add a link to an existing document with implementation details. 
+DataGrid's column data type is specified in the dataType property. If this property is not set it is auto-predicted based on the first row. So, this causes an issue for most customers where their input data is changed into a different data type. (e.g., input a string, but it gets casted to a date/number)
 
 ## Files to Review
 
@@ -29,14 +23,13 @@ Then, add implementation details (steps, code snippets, and other technical info
 - **NetCore**    
     - [Index.cshtml](ASP.NET%20Core/Views/Home/Index.cshtml)
 
+## Implementation Details
+
+Set your dynamic column's dataType property to "object". Then store data type in some data field, specify the editCellTemplate and check the data.Type field's value to render the related editor. Finally, use the setCellValue callback of the Type column to update your dynamic column and rerender the editCellTemplate 
+
 ## Documentation
 
-- link
-- link
-- ...
+- [DataGrid.columns.editCellTemplate](https://js.devexpress.com/Documentation/ApiReference/UI_Components/dxDataGrid/Configuration/columns/#editCellTemplate)
+- [DataGrid.columns.cellTemplate](https://js.devexpress.com/Documentation/ApiReference/UI_Components/dxDataGrid/Configuration/columns/#cellTemplate)
+- [DataGrid.columns.setCellValue](https://js.devexpress.com/Documentation/ApiReference/UI_Components/dxDataGrid/Configuration/columns/#setCellValue)
 
-## More Examples
-
-- link
-- link
-- ...
--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,137 @@
-$(function () {
-    var count = 0;
-    $("#btn").dxButton({
-        text: `Click count: ${count}`,
-        onClick: function(e){
-            count+=1;
-            e.component.option("text", `Click count: ${count}`);
-        }
-    })    
+const typeList = ["String","Number","Date","Boolean", "MyCustomType"]
+const defaultType = "String";
+const defaultValue = "default string";
+
+$(() => {
+  $('#grid-custom-editors').dxDataGrid({
+    dataSource: sampleData,
+    keyExpr: 'ID',
+    onInitNewRow(e) {
+      const newKey = Math.max(...sampleData.map(item=>item.ID)) + 1;
+      e.data.ID = newKey;
+      e.data.Type = defaultType;
+      e.data.DynamicValue = defaultValue;
+    },
+    columns: [
+      {
+        dataField: "ID",
+        allowEditing: false
+      },
+      {
+      dataField: 'DynamicValue',
+      dataType: "object",
+      editCellTemplate(container, cellInfo) {
+        return renderDynamicEditComponent(cellInfo, container);
+      },
+      cellTemplate(container, cellInfo) {
+        const text = getDynamicDisplayText(cellInfo);
+        container.text(text)
+      }
+    }, {
+      dataField: "Type",
+      setCellValue(newData, type){
+        newData.Type = type;
+        newData.DynamicValue = null;
+      },
+      lookup:{
+        dataSource: typeList,
+      }
+    }],
+    showBorders: true,
+    editing: {
+      allowUpdating: true,
+      allowAdding: true,
+      mode: "form"
+    },
+  })
+  
 });
+
+function getDynamicDisplayText(cellInfo) {
+  const type = cellInfo.data.Type,
+        value = cellInfo.data.DynamicValue;
+  const formatterMap = {
+        "_default" : "unknown data type",
+          "String" : value,
+          "Number" : value?.toString(),
+         "Boolean" : value?.toString(),
+            "Date" : new Date(value).toLocaleDateString(),
+    "MyCustomType" : value ? value.Name : ""
+  }
+  return formatterMap[type || "_default"];
+}
+
+function renderDynamicEditComponent(cellInfo, container) {
+  const valueType = cellInfo.data.Type;
+  const editorMap = {
+        "_default" : renderDefault,
+          "String" : renderTextBox,
+          "Number" : renderNumberBox,
+            "Date" : renderDateBox,
+         "Boolean" : renderCheckBox,
+    "MyCustomType" : renderMyCustomEditor
+  }
+  const currentComponent = editorMap[valueType || '_default'];
+  return currentComponent(cellInfo, container);
+}
+
+function renderTextBox(cellInfo, element){
+  element.dxTextBox({
+    value: cellInfo.value,
+    onValueChanged(args) {
+      cellInfo.setValue(args.value);
+    }
+  })
+};
+
+function renderNumberBox(cellInfo, element){
+  element.dxNumberBox({
+    value: cellInfo.value,
+    onValueChanged(args) {
+      cellInfo.setValue(args.value);
+    }
+  })
+};
+
+function renderDateBox(cellInfo, element){
+  element.dxDateBox({
+    value: cellInfo.value,
+    onValueChanged(args) {
+      cellInfo.setValue(args.value);
+    }
+  })
+};
+
+function renderCheckBox(cellInfo, element){
+  element.dxCheckBox({
+    value: cellInfo.value,
+    onValueChanged(args) {
+      cellInfo.setValue(args.value);
+    }
+  })
+};
+
+function renderMyCustomEditor(cellInfo, element){
+  const dataSource = new DevExpress.data.DataSource({
+    store: {
+      type: "array",
+      key: "ID",
+      data: myDropdownData,
+    }
+  });
+  element.dxSelectBox({
+    dataSource: dataSource,
+    value: cellInfo.value ? cellInfo.value.ID : undefined,
+    valueExpr: 'ID',
+    displayExpr: 'Name',
+    onValueChanged(args){
+      const store = args.component.getDataSource().store(),
+            newItem = args.component.option("selectedItem");
+      cellInfo.setValue(newItem);
+    }
+  })
+}
+
+function renderDefault(cellInfo, element){
+  element.text("unknown type");
+}
