883bbea4ef2328d042ee6a86ad7950b3e0ea432d
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-05-15 11:56:32 +0800

    move eslint configs to eslint-config-devextreme

--- jQuery/.eslintrc.js
+++ jQuery/.eslintrc.js
@@ -1,19 +1,6 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
-    'eslint:recommended',
-    'devextreme/javascript',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
+      'devextreme/jquery'
+  ]
 };
