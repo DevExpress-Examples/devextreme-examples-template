dd7b95d693faafd9e76c4782f6d476773351adbf
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-05-25 14:40:00 +0800

    Add linters and tsc (#19)

--- Angular/.eslintrc.js
+++ Angular/.eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   root: true,
   extends: ['devextreme/spell-check'],
   overrides: [{
-    files: ['*.ts', '*.vue'],
-    extends: ['devextreme/vue'],
-    env: { es6: true },
+    files: ['*.ts', '*.html'],
+    extends: ['devextreme/angular'],
+    env: {
+      browser: true,
+      es6: true
+    },
+    parser: 'babel-eslint',
     parserOptions: {
       project: './tsconfig.json',
       'createDefaultProgram': true,
       'ecmaVersion': 6,
-    },
-    globals: {
-      System: false,
-      AzureGateway: false,
-      AzureFileSystem: false,
     },
   }]
 };
