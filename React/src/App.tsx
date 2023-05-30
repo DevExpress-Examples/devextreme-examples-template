f758a4dc73cf987d5bd32d8b5c8d1d21fbffc2b9
Roman Semenov <roman.semenov@devexpress.com>
2023-05-30 09:49:43 +0300

    Linter: Fix simple errors (#21)

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,14 +1,13 @@
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
 import Button from 'devextreme-react/button';
-import { ClickEvent } from 'devextreme/ui/button';
 
-function App() {
+function App(): JSX.Element {
   var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback((e: ClickEvent) => {
-    setCount(prev=>prev + 1);
-  }, [setCount])
+  const clickHandler = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, [setCount]);
   return (
     <div className="main">
       <Button text={`Click count: ${count}`} onClick={clickHandler} />
