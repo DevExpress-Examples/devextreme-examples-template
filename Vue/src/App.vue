9237b1f1cf45d3bbc3ca0bc1388b3c70e5abbbad
williamvinogradov <williamvinogradov@gmail.com>
2024-06-19 17:21:51 +0400

    Update DevExtreme's ESLint config

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-    <div class="main">
-        <RouterView />
-    </div>
+  <div class="main">
+    <RouterView/>
+  </div>
 </template>
