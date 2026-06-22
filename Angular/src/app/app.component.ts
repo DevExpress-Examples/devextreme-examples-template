4d2e74b3c0899767242bbad2cae9c8bed59b3669
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-22 12:09:17 +0300

    Upgrade Angular from 21 to 22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxButtonModule, DxButtonTypes } from 'devextreme-angular/ui/button';
 
 @Component({
     selector: 'app-root',
     imports: [DxButtonModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
